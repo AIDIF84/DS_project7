13ef5d2ff2ca59450c86ecdb848e725864380c85
AIDI FATMA <73826379+AIDIF84@users.noreply.github.com>
2021-08-04 21:51:30 +0200

    Update app_p7.py

--- app_p7.py
+++ app_p7.py
@@ -1,9 +1,21 @@
-import matplotlib.pyplot as plt
 import pickle
 import pandas as pd
 import shap
 import streamlit as st
 import streamlit.components.v1 as components
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import plotly.express as px
+
+import math
+
+
+st.markdown("<h1 style='text-align: center; color: #ff634d;'><strong><u>Home Credit Default Risk</u></strong></h1>",
+            unsafe_allow_html=True)
+
+title_image = Image.open("pret.png")
+st.sidebar.image(title_image)
 
 #loaing model and data
 df_test=pd.read_csv('test.csv')#original data without target
@@ -11,21 +23,19 @@ X_test=pd.read_csv('df_X_test.csv')#data transformed with predict probability an
 # read pickle files
 df_test = df_test.drop(['AMT_TOTAL_RECEIVABLE'], axis=1)
 
-
-#explainer_ = open("explainer.pkl","rb")
-#explainer = pickle.load(explainer_)
-
+#shap values
 shap_values_ = open("shap_values.pkl","rb")
 shap_values = pickle.load(shap_values_)
-
+#features
 features_selected_in = open("features_selected.pkl","rb")
 features_selected = pickle.load(features_selected_in)
-
+#load lodel
 xgb_model_in = open("xgb_model.pkl","rb")
 xgb_model = pickle.load(xgb_model_in)
-
+#explaner shap
 X_shap=X_test[features_selected]
 explainer = shap.TreeExplainer(xgb_model)
+
 #shap_values = explainer(X_shap)
 @st.cache()
 def get_sk_id_list():
@@ -34,27 +44,56 @@ def get_sk_id_list():
     return SK_IDS
 sk_id_list = get_sk_id_list()
 
+
 #select Client ID
-st.sidebar.header("Select ID Client")
+#st.sidebar.header("ID Client")
+st.sidebar.markdown("<h1 style='text-align: center; color: black;'><strong><u>ID Client</u></strong></h1>",
+                    unsafe_allow_html=True)
 sk_id_curr = st.sidebar.selectbox('Select SK_ID from list:', sk_id_list, 0)
-st.write('Client ID: ', sk_id_curr)
+#seuil
+st.sidebar.header("Parametere")
+threshold = st.sidebar.slider(
+        label='Threshold:',
+        min_value=0.,
+        value=0.5,
+        max_value=1.)
 
 #Display caracteristic of Client
 donnee=df_test[df_test['SK_ID_CURR']==sk_id_curr].drop(['Unnamed: 0'], axis=1)
 
-st.subheader('Les Information du client')
+st.subheader('Les informations du client')
 st.write(donnee)
+
 
 st.subheader('Decision')
 prob=X_test[X_test['SK_ID_CURR']==sk_id_curr][['Target_prob','Target_pred']]
-if prob['Target_pred']._values == 0:
-    st.write('### Credit accepté')
+prb=int(prob['Target_prob'].values*100)
+predicted_class = np.where(prb/100 > threshold, 1, 0)
+st.markdown(f"**Default Risk:** {prb}**%**")
+
+if predicted_class == 0:
+    html_temp = """
+        <div style="background-color:#ff634d">
+        <h3 style="color:white;text-align:center;">Crédit Accepté </h3>
+        </div>
+        """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    #st.write('### Credit accepté')
 else:
-    st.write('### Credit rejeté')
+    #st.write('### Credit Rejeté')
+    html_temp2 = """
+            <div style="background-color:orange">
+            <h3 style="color:white;text-align:center;">Crédit Rejeté </h3>
+            </div>
+            """
+    st.markdown(html_temp2, unsafe_allow_html=True)
 
-st.write(prob)
 
-submit = st.button('Get explain')
+st.markdown("<h2 style='text-align: center; color: black;'><strong><u>Interpretabilité</u></strong></h2>",
+            unsafe_allow_html=True)
+
+submit=st.button("Explain")
+
 # explain model prediction results
 def st_shap(plot, height=None):
     shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
@@ -63,10 +102,6 @@ def st_shap(plot, height=None):
 if submit:
     j=prob.index.values
     j=int(j)
-    #st.write(j)
-    #shap.waterfall_plot(explainer.expected_value, shap_values[j], X_shap.iloc[j])
-    #shap.plots.waterfall(shap_values[j])
-    #explainer force_plot
     shap.initjs()
 
     # visualize the first prediction's explanation (use matplotlib=True to avoid Javascript)
@@ -76,17 +111,34 @@ if submit:
     st.subheader('Summary Plot 1')
 
 
-    nb_features=10
-
-    #
+    nb_features=20
     shap.plots.waterfall(shap_values[j])
-    plt.gcf().set_size_inches(14, nb_features / 2)
+    plt.gcf().set_size_inches(16, nb_features / 2)
     # Plot the graph on the dashboard
     st.pyplot(plt.gcf())
 
-    st.subheader('Summary Plot 1')
-    shap.summary_plot(shap_values, X_shap, plot_type="bar")
-    plt.gcf().set_size_inches(14, nb_features / 2)
-    # Plot the graph on the dashboard
-    st.pyplot(plt.gcf())
+st.markdown("<h2 style='text-align: center; color: black;'><strong><u>Analyse</u></strong></h2>",
+            unsafe_allow_html=True)
+#Analyse
+var=['CNT_CHILDREN', 'AMT_CREDIT_x', 'AMT_ANNUITY',
+       'AMT_GOODS_PRICE','DAYS_REGISTRATION', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3',
+       'Diff_day','Diff_pay', 'AMT_CREDIT_y', 'AMT_DOWN_PAYMENT',
+       'NAME_CONTRACT_STATUS_Approved', 'NAME_CONTRACT_STATUS_Canceled',
+       'NAME_CONTRACT_STATUS_Refused', 'REJECT_REASON', 'Status_active',
+       'CNT_INSTALMENT_FUTURE', 'SK_DPD_DEF', 'AMT_DRAWINGS', 'MONTHS_BALANCE',
+       'AMT_TOTAL_RECEIVABLE', 'AMT_CREDIT_LIMIT_ACTUAL', 'AMT_BALANCE',
+       'CREDIT_DAY_OVERDUE', 'DAYS_CREDIT_ENDDATE', 'AMT_CREDIT_SUM',
+       'Nombre_active', 'Comsumer_type_crd', 'Age', 'CREDIT_INCOME',
+       'ANNUITY_INCOME', 'CREDIT_LENGTH', 'YEAR_EMPLOYED_PERCENT',
+       'CREDIT_TO_GOODS_RATIO', 'PAYMENT_RATE', 'ANNUITY_INCOME_PERS']
+
+variable=st.selectbox('choose variable', var)
+fig2 = px.histogram(df_test, x=variable)
+
+x_val=int(donnee[variable].values)
+fig2.add_vline(x=x_val, line_width=3, line_dash="dash", line_color="green",
+               annotation_text="ID_Client: "+str(x_val),
+               annotation_position="top right",)
+st.plotly_chart(fig2)
+
 
