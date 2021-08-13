d658714854749ae2a344bbb422f973e64aafb8d5
AIDI FATMA <73826379+AIDIF84@users.noreply.github.com>
2021-08-13 17:14:09 +0200

    Update app_p7.py

--- app_p7.py
+++ app_p7.py
@@ -143,8 +143,8 @@ var=['CNT_CHILDREN', 'AMT_CREDIT_x', 'AMT_ANNUITY',
 variable=st.selectbox('Choisir la variable', var)
 fig2 = px.histogram(df_test, x=variable)
 
-x_val=int(donnee[variable].values)
-fig2.add_vline(x=x_val, line_width=3, line_dash="dash", line_color="green",
+x_val=int(donnee[variable])#.values)
+fig2.add_vline(x=donnee[variable], line_width=3, line_dash="dash", line_color="green",
                annotation_text="ID_Client: "+str(x_val),
                annotation_position="top right",)
 st.plotly_chart(fig2)
